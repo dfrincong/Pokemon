cc0f50ac85682484ade0b79ba79038ecc2be9441
dfrincong <139162742+dfrincong@users.noreply.github.com>
2023-09-25 20:41:41 -0500

    feat: :sparkles: se agrega funciones para mostrar pokemones

--- main.js
+++ main.js
@@ -1,35 +1,44 @@
-// Swal.fire({
-//     icon: 'error',
-//     title: 'Oops...',
-//     text: 'Something went wrong!',
-//     footer: '<a href="">Why do I have this issue?</a>'
-//   })
+// let myPokemon = document.querySelector("#myPokemon");
 
-let myPikachu = document.querySelector("#myPikachu");
+// myPokemon.addEventListener("click", async()=> {
+//     let res = await (await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")).json();
+//     let img = res.sprites.front_default;
+//     let defaultImg = "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif" 
 
-myPikachu.addEventListener("click", async()=> {
-    let res = await (await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")).json();
-    let img = res.sprites.front_default;
-    let defaultImg = "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif" 
+//     Swal.fire({
+//         title: `${res.name}`,
+//         text: 'Modal with a custom image.',
+//         // imageUrl: `${res.sprites.front_default}`,
+//         imageUrl:  `${(img) ?  img : defaultImg}`,
+//         html: `
+//             ${res.stats.map(data=>`
+//                 <input 
+//                     type="range" 
+//                     id="uno" 
+//                     value="${data.base_stat}">
+//                 <label for="uno"> 
+//                     ${data.base_stat} 
+//                     ${data.stat.name}</label><br>
+//                     `).join("")}   
+//         `,
+//         imageWidth: "80%",
+//         imageHeight: "80%",
+//     });
+// });
 
+const url = "https://pokeapi.co/api/v2/pokemon/";
+let myPokemon = document.querySelector("#myPokemon");
 
-    Swal.fire({
-        title: `${res.name}`,
-        text: 'Modal with a custom image.',
-        // imageUrl: `${res.sprites.front_default}`,
-        imageUrl:  `${(img) ?  img : defaultImg}`,
-        html: `
-            ${res.stats.map(data=>`
-                <input 
-                    type="range" 
-                    id="uno" 
-                    value="${data.base_stat}">
-                <label for="uno"> 
-                    ${data.base_stat} 
-                    ${data.stat.name}</label><br>
-                    `).join("")}   
-        `,
-        imageWidth: "80%",
-        imageHeight: "80%",
-    });
-});
+const cambiarId = (id)=>{
+    fetch(url + `${id}/`)
+        .then(res => res.json())
+        .then(data => console.log(data));
+};
+
+const mostrar = (cantidad)=>{
+    for (let i = 1; i <= cantidad; i++) {
+        cambiarId(i)
+    }
+};
+
+mostrar(3);
