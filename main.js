cafe117143f30cb21ebffc5f5829bd2aa6c2d9a7
dfrincong <139162742+dfrincong@users.noreply.github.com>
2023-09-27 07:22:07 -0500

    fix: :bug: se arregla un error
    se arregla el error en el que se mostraba el último pokemon en todos los botones
    
    (74adc082)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const myPokemones = document.querySelector(".cajaPokemones");
 
 const cambiarId = async(id)=>{
     let data = await (await fetch(url + `${id}/`)).json();
-    console.log(data);
+    // console.log(data);
     mostrarPokemon(data);
 };
 
@@ -19,38 +19,39 @@ const mostrarPokemon = (pokemon)=>{
         <div class="cajaImagen">
             <img src="${pokemon.sprites.front_default}">
         </div>
-        <button class="nombre">${pokemon.name}</button>
+        <button class="nombre" data-name="${pokemon.name}">${pokemon.name}</button>
     </div>`);
-
-    const click = document.querySelectorAll(".nombre");
-    console.log(click);
-    click.forEach(element => {
-        console.log(element);
-        element.addEventListener("click",()=>{
-            let img = pokemon.sprites.other.home.front_default;
-            let defaultImg = "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif";
-            Swal.fire({
-                title: `${pokemon.name}`,
-                text: 'Modal with a custom image.',
-                imageUrl:  `${(img) ?  img : defaultImg}`,
-                html: `
-                    ${pokemon.stats.map(data=>`
-                        <input 
-                            type="range" 
-                            id="${data.stat.name}" 
-                            value="${data.base_stat}"
-                            max="200">
-                        <label for="${data.stat.name}"> 
-                            ${data.base_stat} 
-                            ${data.stat.name}</label><br>
-                            `).join("")}   
-                `,
-                imageWidth: "60%",
-                imageHeight: "80%",
-                background: "lightblue",
-            });
-        });
-    });
 };
+
+document.addEventListener("click", async (e)=>{
+    // console.log(e.target);
+    if(e.target.matches(".nombre")){
+        let pokemon = await (await fetch(url + `${e.target.dataset.name}/`)).json();
+        let img = pokemon.sprites.other.home.front_default;
+        let defaultImg = "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif";
+        Swal.fire({
+            title: `${pokemon.name}`,
+            imageUrl:  `${(img) ?  img : defaultImg}`,
+            html: `
+                ${pokemon.stats.map(data=>`
+                    <input 
+                        type="range" 
+                        id="${data.stat.name}" 
+                        value="${data.base_stat}"
+                        max="200">
+                    <label for="${data.stat.name}"> 
+                        ${data.base_stat} 
+                        ${data.stat.name}</label><br>
+                        `).join("")}   
+            `,
+            imageWidth: "60%",
+            imageHeight: "80%",
+            background: "lightblue",
+        });
+    }
+
+})
+
+
 
 mostrar(7);
